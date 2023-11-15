9d7eeb05eff6e0d17de2264bd779f4ad16b918a3
TheDemopan <87335546+TheDemopan@users.noreply.github.com>
2023-11-15 18:10:45 -0500

    Updated path and status labels

--- capture.py
+++ capture.py
@@ -3,16 +3,16 @@ import time
 import os
 import uuid
 
-IMAGE_PATH = '/home/suyash/Images/raw-set'
+IMAGE_PATH = 'C:\\Users\\srisu\\OneDrive\\Documents\\Laptop-tkd-dev\\Taekwondo-Coach-Rev'
 
 # labels, which I will be using as statuses,
 # describe common errors made when making a fist
-labels = ['correct', 'tuck in your thumb', 'your thumb should be under your fingers']
+labels = ['correct', 'tuck-in-your-thumb', 'your-thumb-should-be-under-your-fingers']
 numberOfImages = 30
 
 for status in labels:
     c = 0
-    os.mkdir('/home/suyash/Images/raw-set/' + status) # makes directory with name of status/labels
+    os.mkdir('C:\\Users\\srisu\\OneDrive\\Documents\\Laptop-tkd-dev\\Taekwondo-Coach-Rev' + status) # makes directory with name of status/labels
     cap = cv2.VideoCapture(0) # 0 for built in webcam, 1 for external
     time.sleep(5) # wait 5 seconds
     for img in range(numberOfImages): # for every image in the predefined range (30)
