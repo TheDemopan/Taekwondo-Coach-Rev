6bed6f3ee94d5258df32ad686b649cc21fb2838b
TheDemopan <87335546+TheDemopan@users.noreply.github.com>
2023-11-15 20:31:44 -0500

    edits to script

--- capture.py
+++ capture.py
@@ -1,24 +1,24 @@
 import cv2
 import time
 import os
-import uuid
-
-IMAGE_PATH = 'C:\\Users\\srisu\\OneDrive\\Documents\\Laptop-tkd-dev\\Taekwondo-Coach-Rev'
 
 # labels, which I will be using as statuses,
 # describe common errors made when making a fist
 labels = ['correct', 'tuck-in-your-thumb', 'your-thumb-should-be-under-your-fingers']
-numberOfImages = 30
+numberOfImages = 15
+parentDir = 'C:\\Users\\srisu\\OneDrive\\Documents\\Laptop-tkd-dev\\Taekwondo-Coach-Rev\\raw-capture'
 
 for status in labels:
     c = 0
-    os.mkdir('C:\\Users\\srisu\\OneDrive\\Documents\\Laptop-tkd-dev\\Taekwondo-Coach-Rev' + status) # makes directory with name of status/labels
+    dir = os.path.join(parentDir, status) # makes directory with name of status/labels
+    os.mkdir(dir)
+    os.chdir(dir)
     cap = cv2.VideoCapture(0) # 0 for built in webcam, 1 for external
     time.sleep(5) # wait 5 seconds
     for img in range(numberOfImages): # for every image in the predefined range (30)
-        print("Image " + str(c) + "/30 for status {}".format(status)) # print the status log
+        print("Image " + str(c) + "/15 for status {}".format(status)) # print the status log
         ret, frame = cap.read()
-        fileName = os.path.join(IMAGE_PATH, status, status + '.' + '{}.jpg'.format(str(uuid.uuid1()))) # save the image with this name
+        fileName = dir + '-' + '{}.jpg'.format(str(c)) # save the image with this name
         cv2.imwrite(fileName, frame)
         cv2.imshow('frame', frame)
         time.sleep(3) # wait 3 seconds
