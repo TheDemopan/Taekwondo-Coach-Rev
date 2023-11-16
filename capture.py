2b6299cd9a070dfedf0b7c7858623a40859e3912
TheDemopan <87335546+TheDemopan@users.noreply.github.com>
2023-11-15 21:18:51 -0500

    Sample images + tools
    Included tailored model by nicknochnack and LabelImg

--- capture.py
+++ capture.py
@@ -5,8 +5,8 @@ import os
 # labels, which I will be using as statuses,
 # describe common errors made when making a fist
 labels = ['correct', 'tuck-in-your-thumb', 'your-thumb-should-be-under-your-fingers']
-numberOfImages = 15
-parentDir = 'C:\\Users\\srisu\\OneDrive\\Documents\\Laptop-tkd-dev\\Taekwondo-Coach-Rev\\raw-capture'
+numberOfImages = 16
+parentDir = 'C:/Users/User/Documents/GitHub/Taekwondo-Coach/raw-capture'
 
 for status in labels:
     c = 0
@@ -15,12 +15,13 @@ for status in labels:
     os.chdir(dir)
     cap = cv2.VideoCapture(0) # 0 for built in webcam, 1 for external
     time.sleep(5) # wait 5 seconds
-    for img in range(numberOfImages): # for every image in the predefined range (30)
-        print("Image " + str(c) + "/15 for status {}".format(status)) # print the status log
+    for img in range(numberOfImages): # for every image in the predefined range
+        print("Image " + str(c) + "/16 for status [{}]".format(status)) # print the status log
         ret, frame = cap.read()
         fileName = dir + '-' + '{}.jpg'.format(str(c)) # save the image with this name
         cv2.imwrite(fileName, frame)
         cv2.imshow('frame', frame)
+        c+=1
         time.sleep(3) # wait 3 seconds
 
         if (cv2.waitKey(1) & 0xFF == ord('q')):
